347455442f70f5086ba45cb925ed29e3d1d99d53
Karin Abe <karin.abe@gmail.com>
2025-04-15 15:27:23 -0300

    Componente - Acompanhamentos

--- cardapio/src/App.js
+++ cardapio/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "./Styles/App.scss";
 import Header from "./Components/Header";
+import Acompanhamentos from "./Components/Acompanhamentos";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Acompanhamentos />
     </div>
   );
 }
